884e691f1f8de7f743f8c1ad8934bbd8668771d4
dhanya <dsridhar1@luc.edu>
2024-12-03 17:52:47 -0600

    Dhanya Sridhar - changes

--- total.js
+++
@@ -1,9 +0,0 @@
-        // Function to get URL parameters
-        function getQueryParameter(name) {
-                const urlParams = new URLSearchParams(window.location.search);
-                return urlParams.get(name);
-     }      
-      
-      // Display the total from the URL
-        var totalValue = getQueryParameter("total");
-        document.getElementById("total-display").innerText = totalValue;
